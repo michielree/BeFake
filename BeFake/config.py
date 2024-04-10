f379347f5389f9b26d4102e7093bf0969bf5391d
Michiel van der Ree <michiel.van.der.ree@rug.nl>
2024-04-10 15:03:33 +0200

    Update Google API key

--- BeFake/config.py
+++ BeFake/config.py
@@ -33,7 +33,7 @@ CONFIG = {
         },
     },
     "google": {
-        "api-key": "AIzaSyDwjfEeparokD7sXPVQli9NsTuhT6fJ6iA",
+        "api-key": "AIzaSyCgNTZt6gzPMh-2voYXOvrt_UR_gpGl83Q",
         "appToken": "54F80A258C35A916B38A3AD83CA5DDD48A44BFE2461F90831E0F97EBA4BB2EC7",
     },
 }
